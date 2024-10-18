64ada04f52e97f1d753e4f96a2c08ceb8ef289a8
Yesenia Salas <yesisalas1804@gmail.com>
2024-10-18 17:11:48 -0300

    Novo arquivo NASM

---
+++ soma.asm
@@ -0,0 +1,17 @@
+section .data
+    num1 dq 10
+    num2 dq 20
+    result dq 0
+
+section .text
+    globaL _start
+
+_start:
+    mov rax, [num1]     
+    add rax , [num2]
+    mov [result], rax
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
